12727d42da27dcb130352865ab7452a12a899e3a
XinHallow <xinhallow@outlook.com>
2025-12-05 21:33:04 +0800

    feat(server/character): 添加 `四宫辉夜`

--- server/src/data/characters/january.ts
+++ server/src/data/characters/january.ts
@@ -1,10 +1,16 @@
+import { characterDataUri as p } from "@/utils";
 import type { ICharacter } from ".";
 
 export default [
   {
-    name: "example",
+    name: "四宫辉夜",
     birthday: { month: 1, day: 1 },
-    from: "example",
-    assets: {},
+    from: "辉夜大小姐想让我告白",
+    assets: {
+      avatar: p("四宫辉夜", "avatar.png"),
+      avatarFrom: "Official",
+      background: p("四宫辉夜", "background.png"),
+      backgroundFrom: "Unknown",
+    },
   },
 ] satisfies ICharacter[];
