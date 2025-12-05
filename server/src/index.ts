0b07cfc01b3818f53c48d498f22242987b097feb
XinHallow <xinhallow@outlook.com>
2025-12-05 21:15:49 +0800

    feat(server): 添加静态资源路由插件

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,12 @@ import { Elysia } from "elysia";
 import openapi, { fromTypes } from "@elysiajs/openapi";
 import { route as charactersRoute } from "./routes/characters";
 import config from "./config";
+import { staticPlugin } from "@elysiajs/static";
 
 const app = new Elysia()
+  .use(
+    staticPlugin({ assets: "./public", prefix: "/public", alwaysStatic: true }),
+  )
   .use(openapi({ references: fromTypes() }))
   .group("/api/characters", (app) => app.use(charactersRoute))
   .listen(config.SERVER_PORT);
