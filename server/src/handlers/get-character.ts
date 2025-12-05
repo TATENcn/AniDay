5675ad0b1f9ca70139a7d06cd35e4bba505e513b
XinHallow <xinhallow@outlook.com>
2025-12-05 20:18:45 +0800

    feat(server): 添加获取 character 的 handler 和 route

---
+++ server/src/handlers/get-character.ts
@@ -0,0 +1,15 @@
+import { characters, type ICharacter } from "@/data/characters";
+
+const getByDate = (month: number, day: number): ICharacter[] =>
+  characters.filter(
+    ({ birthday }) => birthday.day === day && birthday.month === month,
+  );
+
+const getToday = (): ICharacter[] => {
+  const date = new Date();
+  const [month, day] = [date.getMonth() + 1, date.getDate()];
+
+  return getByDate(month, day);
+};
+
+export { getByDate, getToday };
---
+++ server/src/routes/characters.ts
@@ -0,0 +1,42 @@
+import Elysia, { t } from "elysia";
+import { characterSchema } from "@/data/characters";
+import { getByDate, getToday } from "@/handlers/get-character";
+
+const characterResponseSchema = t.Object({
+  count: t.Number({ minimum: 0 }),
+  characters: t.Array(characterSchema),
+});
+
+const route = new Elysia()
+  .get(
+    "/today",
+    () => {
+      const characters = getToday();
+
+      return {
+        count: characters.length,
+        characters: characters,
+      };
+    },
+    { response: characterResponseSchema },
+  )
+  .get(
+    "/:month/:day",
+    ({ params }) => {
+      const characters = getByDate(params.month, params.day);
+
+      return {
+        count: characters.length,
+        characters: characters,
+      };
+    },
+    {
+      params: t.Object({
+        month: t.Numeric({ maximum: 12, minimum: 1 }),
+        day: t.Numeric({ maximum: 31, minimum: 1 }),
+      }),
+      response: characterResponseSchema,
+    },
+  );
+
+export { route };
