dcc4de7bf7210da3141f62b142ec625ba512eae0
XinHallow <xinhallow@outlook.com>
2025-12-06 21:07:50 +0800

    feat(website): 添加 tailwindcss 支持

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,7 +1,8 @@
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "@tanstack/react-router";
+import { createRoot } from "react-dom/client";
+import "@/assets/global.css";
+import router from "./router";
 
 // biome-ignore lint/style/noNonNullAssertion: index.html 拥有 #root 元素
 const element = document.querySelector("#app")!;
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackRouter({ enableRouteGeneration: false }),
     viteReact(),
+    tailwindcss(),
     tsconfigPaths(),
     devtools(),
   ],
