44b8802c9cf6e4a742771a849383660ce68077b9
XinHallow <xinhallow@outlook.com>
2025-12-07 10:32:34 +0800

    feat(server, website): 添加默认配置

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,8 +2,8 @@ import { t } from "elysia";
 import { compile } from "elysia/type-system/utils";
 
 const schema = t.Object({
-	SERVER_PORT: t.Numeric(),
-	SERVER_HOST: t.String(),
+	SERVER_PORT: t.Numeric({ default: 3000 }),
+	SERVER_HOST: t.String({ default: "0.0.0.0" }),
 });
 
 export type Schema = typeof schema.static;
--- website/src/config.ts
+++ website/src/config.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 const configSchema = z.object({
-	VITE_API_BASE_URL: z.string(),
+	VITE_API_BASE_URL: z.string().default("http://localhost:3000"),
 });
 
 export default configSchema.parse(import.meta.env);
